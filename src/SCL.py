289ae3fc8a421594cb6e942969165b089ae17f20
0LGL0 <artemkohan36@gmail.com>
2026-01-17 17:19:33 +0300

    First working commit without brakes

---
+++ src/SCL.py
@@ -0,0 +1,45 @@
+def calculate(rawString):
+    i = 0
+    arr = list()
+    numStr = ""
+    while i < len(rawString):
+        c = rawString[i]
+        if c.isdigit() or c == ".":
+            numStr += c
+        else:
+            arr.append(numStr)
+            arr.append(c)
+            numStr = ""
+        i += 1
+    arr.append(numStr)
+    i = 1
+
+    while i < len(arr) - 1:
+        left = arr[i - 1]
+        mid = arr[i]
+        right = arr[i + 1]
+
+        divMulRes = 0
+        if mid in "*/":
+            if mid == "*":
+                divMulRes = float(left) * float(right)
+            else:
+                divMulRes = float(left) / float(right)
+
+            arr.pop(i + 1)
+            arr.pop(i)
+            arr.pop(i - 1)
+            arr.insert(i - 1, divMulRes)
+
+        i += 1
+    i = 0
+
+    res = float(arr[0])
+    while i < len(arr):
+        if arr[i] == "+":
+            res += float(arr[i + 1])
+        elif arr[i] == "-":
+            res -= float(arr[i + 1])
+        i += 1
+
+    return res
